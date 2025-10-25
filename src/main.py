c7452dbf1b9e296d09de0b217bf2966dd2256f9b
two4faced <jyna5li@gmail.com>
2025-10-25 10:31:53 +0300

    adding main files

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+from src.api.hotels import router as router_hotels
+
+app = FastAPI()
+
+app.include_router(router_hotels)
+
+
+if __name__ == '__main__':
+    uvicorn.run('__main__:app', reload=True)
