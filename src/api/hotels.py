d775d1ff66078d73afe44523c617bb8691f646d7
two4faced <jyna5li@gmail.com>
2025-10-31 13:08:50 +0300

    add hotels in database

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,7 +1,11 @@
 from fastapi import Query, APIRouter, Body
 
-from src.schemas.hotels import Hotel, HotelPatch
+from sqlalchemy import insert
+
 from src.api.dependencies import PaginationDep
+from src.database import async_session_maker
+from src.models.hotels import HotelsORM
+from src.schemas.hotels import Hotel, HotelPatch
 
 router = APIRouter(prefix='/hotels', tags=['Отели'])
 
@@ -37,12 +41,13 @@ def get_hotels(
 
 
 @router.post('', summary='Добавить отель')
-def create_hotel(hotel_data: Hotel = Body(openapi_examples={
+async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     "1": {
         "summary": "Сочи",
         "value": {
             "title": "Отель Сочи 5 звезд у моря",
             "name": "sochi_u_morya",
+            "location": "ул. Моря, 1",
         }
     },
     "2": {
@@ -50,16 +55,16 @@ def create_hotel(hotel_data: Hotel = Body(openapi_examples={
         "value": {
             "title": "Отель Дубай У фонтана",
             "name": "dubai_fountain",
+            "location": "ул. Шейха, 2",
         }
     }
 })
 ):
-    global hotels
-    hotels.append({
-        'id': hotels[-1]['id'] + 1,
-        'title': hotel_data.title,
-        'name': hotel_data.name
-    })
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsORM).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
+
     return {'status': 'OK'}
 
 
