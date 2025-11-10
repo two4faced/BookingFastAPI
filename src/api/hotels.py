29213a427f8f74f62e0a19f32e343f0ada83c75e
two4faced <jyna5li@gmail.com>
2025-11-10 17:10:59 +0300

    added get_hotel method

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -25,6 +25,12 @@ async def get_hotels(
         )
 
 
+@router.get('hotels/{hotel_id}', summary='Получить один отель')
+async def get_hotel(hotel_id: int):
+    async with async_session_maker() as session:
+        return await HotelsRepository(session).get_one_or_none(id=hotel_id)
+
+
 @router.post('', summary='Добавить отель')
 async def create_hotel(hotel_data: Hotel = Body(openapi_examples={
     "1": {
