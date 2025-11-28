0679762971eedceaaff60f1c91d308fa6324aa02
two4faced <jyna5li@gmail.com>
2025-11-28 11:51:44 +0300

    fix price in book_room method

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -10,7 +10,7 @@ router = APIRouter(prefix='/bookings', tags=['Бронирования'])
 async def book_room(request: Request, booking_data: BookingsAddRequest, db: DBDep):
     user_id = get_current_user_id(get_token(request))
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
-    price = room.model_dump().get('price')
+    price = room.model_dump().get('price') * (booking_data.date_to - booking_data.date_from).days
     _booking = BookingsAdd(user_id=user_id, price=price, **booking_data.model_dump())
     res = await db.bookings.add(_booking)
     await db.commit()
