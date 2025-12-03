1a07c47e13ba21ff917a113882adfa74305e1fa1
two4faced <jyna5li@gmail.com>
2025-12-03 14:01:28 +0300

    change path in api/facilities.py

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -3,18 +3,18 @@ from fastapi import APIRouter
 from src.api.dependencies import DBDep
 from src.schemas.facilities import FacilityAdd
 
-router = APIRouter(prefix='/rooms', tags=['Удобства'])
+router = APIRouter(prefix='/facilities', tags=['Удобства'])
 
 
-@router.get('/facilities')
+@router.get('')
 async def get_all_facilities(db: DBDep):
     return await db.facilities.get_all()
 
 
 
-@router.post('/facilities')
-async def add_facility(data: FacilityAdd, db: DBDep):
-    result = await db.facilities.add(data)
+@router.post('')
+async def add_facility(facility_data: FacilityAdd, db: DBDep):
+    result = await db.facilities.add(facility_data)
     await db.commit()
 
     return {'status': 'OK', 'data': result}
