93af4228a3f71369ba3d6a032174727942581952
two4faced <jyna5li@gmail.com>
2025-12-24 10:32:46 +0300

    add fixture for creating db

--- tests/integration/hotels/test_db.py
+++ tests/integration/hotels/test_db.py
@@ -6,4 +6,5 @@ async def test_add_hotel():
     hotel_data = HotelAdd(title='Отель 5 звёзд', location='г. Краснодар, ул. Красная, д. 134')
     async with get_db_manager() as db:
         new_hotel_data = await db.hotels.add(hotel_data)
+        await db.commit()
         print(f'{new_hotel_data=}')
