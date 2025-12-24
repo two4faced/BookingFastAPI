8ed390d6855770235194bd979a0ed90a5c41e77c
two4faced <jyna5li@gmail.com>
2025-12-24 11:00:23 +0300

    use httpx to register user

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,30 @@
 import pytest
+from httpx import AsyncClient, ASGITransport
 
 from src.config import settings
 from src.database import Base, engine_null_pool
+from src.main import app
 from src.models import *
 
 
 @pytest.fixture(scope="session", autouse=True)
 def check_test_mode():
     assert settings.MODE == "TEST"
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url='http://test') as ac:
+        await ac.post(
+            '/auth/register',
+            json={
+                'name': 'pytest',
+                'nickname': 'pytest',
+                'email': 'coolemail@mail.ru',
+                'password': 'pytest1234'
+            }
+        )
+
 
 
 @pytest.fixture(scope="session", autouse=True)
--- tests/integration/hotels/test_db.py
+++ tests/integration/hotels/test_db.py
@@ -1,10 +1,11 @@
-from src.api.dependencies import get_db_manager
+from src.database import async_session_maker_null_pool
 from src.schemas.hotels import HotelAdd
+from src.utils.db_manager import DBManager
 
 
 async def test_add_hotel():
     hotel_data = HotelAdd(title='Отель 5 звёзд', location='г. Краснодар, ул. Красная, д. 134')
-    async with get_db_manager() as db:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
         new_hotel_data = await db.hotels.add(hotel_data)
         await db.commit()
         print(f'{new_hotel_data=}')
