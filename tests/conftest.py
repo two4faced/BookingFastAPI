da842daaa29c8b5cca2c117e70d40a79317f4075
two4faced <jyna5li@gmail.com>
2025-12-25 08:56:26 +0300

    add mock data to database for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
+import json
 
 from src.config import settings
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker_null_pool
 from src.main import app
 from src.models import *
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomsAdd
+from src.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -13,7 +17,36 @@ def check_test_mode():
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def register_user():
+async def setup_database(check_test_mode):
+    async with engine_null_pool.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def add_data(setup_database):
+    with open('tests/mock_hotels.json', 'r', encoding='utf-8') as data:
+        data_hotels = json.load(data)
+
+    with open('tests/mock_rooms.json', 'r', encoding='utf-8') as data:
+        data_rooms = json.load(data)
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        for hotel in data_hotels:
+            await db.hotels.add(HotelAdd(title=hotel['title'], location=hotel['location']))
+        for room in data_rooms:
+            await db.rooms.add(RoomsAdd(
+                title=room['title'],
+                description=room['description'],
+                price=room['price'],
+                quantity=room['quantity'],
+                hotel_id=room['hotel_id'])
+            )
+        await db.commit()
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(add_data):
     async with AsyncClient(transport=ASGITransport(app=app), base_url='http://test') as ac:
         await ac.post(
             '/auth/register',
@@ -24,11 +57,3 @@ async def register_user():
                 'password': 'pytest1234'
             }
         )
-
-
-
-@pytest.fixture(scope="session", autouse=True)
-async def async_main(check_test_mode):
-    async with engine_null_pool.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
--- tests/integration/hotels/test_db.py
+++ tests/integration/hotels/test_db.py
@@ -8,4 +8,3 @@ async def test_add_hotel():
     async with DBManager(session_factory=async_session_maker_null_pool) as db:
         new_hotel_data = await db.hotels.add(hotel_data)
         await db.commit()
-        print(f'{new_hotel_data=}')
