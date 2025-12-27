461362ce661a59ab7bad2f491f13345ed857d6ce
two4faced <jyna5li@gmail.com>
2025-12-27 12:10:06 +0300

    refactor POST facilities test

--- tests/integration/facilities/test_api.py
+++ tests/integration/facilities/test_api.py
@@ -8,13 +8,19 @@ async def test_get_facilities(ac):
     assert response.status_code == 200
 
 
-async def test_add_facility(ac, db):
+async def test_post_facilities(ac, db):
+    facility_title = 'SPA'
     response = await ac.post(
         '/facilities',
         json={
-            'title': 'SPA'
+            'title': facility_title
         }
     )
 
     assert response.status_code == 200
-    assert db.facilities.get_one_or_none(title='SPA')
+    assert db.facilities.get_one_or_none(title=facility_title)
+
+    result = response.json()
+
+    assert 'data' in result
+    assert result['data']['title'] == facility_title
