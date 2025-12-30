52d6e7e9fe89e622a032593fa1e1da64cace5690
two4faced <jyna5li@gmail.com>
2025-12-30 12:07:41 +0300

    add test_add_booking

--- tests/integration/bookings/test_api.py
+++ tests/integration/bookings/test_api.py
@@ -1,16 +1,35 @@
-async def test_add_booking(db, authenticated_ac):
-    room_id = (await db.rooms.get_all())[0].id
+import pytest
+
+
+@pytest.mark.parametrize('room_id, date_from, date_to, status_code', [
+    (1, "2024-08-01", "2024-08-10", 200),
+    (1, "2024-08-02", "2024-08-11", 200),
+    (1, "2024-08-03", "2024-08-12", 200),
+    (1, "2024-08-04", "2024-08-13", 200),
+    (1, "2024-08-05", "2024-08-14", 200),
+    (1, "2024-08-06", "2024-08-15", 404),
+    (1, "2024-08-17", "2024-08-25", 200),
+                         ])
+async def test_add_booking(
+        room_id: int,
+        date_from: str,
+        date_to: str,
+        status_code: int,
+        db,
+        authenticated_ac
+):
+    # room_id = (await db.rooms.get_all())[0].id
     response = await authenticated_ac.post(
         '/bookings',
         json={
             'room_id': room_id,
-            'date_from': '2024-08-01',
-            'date_to': '2024-08-10',
+            'date_from': date_from,
+            'date_to': date_to,
         }
     )
     result = response.json()
 
-    assert response.status_code == 200
-    assert 'data' in result
-    assert result['status'] == 'OK'
-    assert isinstance(result, dict)
+    assert response.status_code == status_code
+    if status_code == 200:
+        assert 'data' in result
+        assert isinstance(result, dict)
