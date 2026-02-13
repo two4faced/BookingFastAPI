099917bbe7eddf7216248c3890fe5a37f0c38b16
two4faced <jyna5li@gmail.com>
2026-02-13 19:05:28 +0300

    add GET ratings

--- src/api/ratings.py
+++ src/api/ratings.py
@@ -13,6 +13,11 @@ from src.services.ratings import RatingService
 router = APIRouter(prefix='/ratings', tags=['Отзывы'])
 
 
+@router.get('/{hotel_id}')
+async def get_ratings(hotel_id: int, db: DBDep):
+    return await RatingService(db).get_ratings(hotel_id)
+
+
 @router.post('/{hotel_id}')
 async def add_rating(data: RatingRequestAdd, hotel_id: int, user_id: UserIdDep, db: DBDep):
     try:
